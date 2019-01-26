966ff47aefefa372cd6e8a9496ae2ff681e52241
Darkantheos <tim-miloche@orange.fr>
2019-01-26 11:45:15 +0100

    tim Debut Grid + Tuiles instance

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -13,8 +13,35 @@ public class Master : MonoBehaviour
     public GameObject TuileParent;
     public Vector3 vectorTemp = new Vector3(0,0,0);
     // Start is called before the first frame update
+
+    public GameObject LinePrefab;
+    public Transform GridParent; 
+
+    public int gridSize = 10;
+        public void DrawGrid()
+    {
+        //Dessinne la grille en lines renderer
+        for (int i = 0; i < gridSize +1; i++)
+        {
+            GameObject Line = Instantiate(LinePrefab, GridParent);
+            vectorTemp.Set(i*5 - 2.5f, 0.1f, -2.5f);
+            Line.GetComponent<LineRenderer>().SetPosition(0, vectorTemp);
+            vectorTemp.Set(i * 5 - 2.5f, 0.1f, 47.5f);
+            Line.GetComponent<LineRenderer>().SetPosition(1, vectorTemp);
+        }
+        for (int j = 0; j < gridSize + 1; j++)
+        {
+            GameObject Line = Instantiate(LinePrefab, GridParent);
+            vectorTemp.Set(-2.5f, 0.1f, j*5 - 2.5f);
+            Line.GetComponent<LineRenderer>().SetPosition(0, vectorTemp);
+        }
+    }
+
+
+
     void Start()
     {
+        DrawGrid();
         TuilesMap = new List<Tuile>();
         FillTuiles();
         InstantiateTuiles();
@@ -27,7 +54,9 @@ public class Master : MonoBehaviour
             for (int j = 0; j < 10; j++)
             {
                 Tuile tuile = new Tuile();
-                tuile.CoordTuile = new Vector2(i, j);
+                tuile.CoordTuile = new Vector2(i*5, j*5);
+                TuilesMap.Add(tuile);
+              
             }
         }
     }
@@ -42,9 +71,9 @@ public class Master : MonoBehaviour
     {
         for (int i = 0; i < TuilesMap.Count; i++)
         {
-            vectorTemp.Set(TuilesMap[i].CoordTuile.x, TuilesMap[i].CoordTuile.y, 0);
+            vectorTemp.Set(TuilesMap[i].CoordTuile.x, 0, TuilesMap[i].CoordTuile.y);
             GameObject tuile = Instantiate(TuilePrefab, vectorTemp, Quaternion.identity ,TuileParent.transform);
-               
+              
         }
     }
 }
