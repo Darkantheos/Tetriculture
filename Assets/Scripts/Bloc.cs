55a5aa31c788948fc45da575068083e2d9c4dc57
Mellange29 <47043253+Mellange29@users.noreply.github.com>
2019-01-27 00:00:00 +0100

    BlocObject

--- Assets/Scripts/Bloc.cs
+++ Assets/Scripts/Bloc.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 [System.Serializable]
-public class Bloc : MonoBehaviour
+public class Bloc
 {
     // Une case en L est definie par 4 cases avec ces positions :
     // (0,0) / (1,0) / (0,1) / (0,2)
--- Assets/Scripts/TestInventaire.cs
+++ Assets/Scripts/TestInventaire.cs
@@ -10,48 +10,17 @@ public class TestInventaire : MonoBehaviour
     [SerializeField] private Canvas canvas;
     [SerializeField] private RectTransform content;
 
-    [SerializeField] private TestGrille grille;
+   
 
-    private Bloc blocCourant = null;
-    private Vector3 blocPosition;
-    private Vector3 blocRotation;
+    
 
 
     // Start is called before the first frame update
     private void Start()
     {
-        blocPosition = new Vector3(0, 0, 0);
-        blocRotation = new Vector3(0, 0, 0);
-    }
-
-    private void Update()
-    {
-        //Debug.Log(Input.mousePosition);
-
-        if((blocCourant) && (Input.GetMouseButton(0)))
-        {
-            blocPosition.Set(Input.mousePosition.x, Input.mousePosition.y, 2);
-            blocPosition = Camera.main.ScreenToWorldPoint(blocPosition);
-            blocPosition.Set(blocPosition.x, blocPosition.y, 1.9f);
-            blocCourant.transform.position = blocPosition;
-        }
-
-        if ((blocCourant) && (Input.GetMouseButtonUp(0)))
-        {
-            grille.VerifierContenu(blocPosition, (int)blocRotation.z);
-
-            DestroyImmediate(blocCourant.gameObject);
-            blocCourant = null;
-
-            blocPosition.Set(0, 0, 0);
-            blocRotation.Set(0, 0, 0);
-        }
+        
     }
 
 
-    public void CreerBlocL()
-    {
-        blocRotation.Set(0, 0, contenus[0].GetRotation());
-        blocCourant = Instantiate(blocs[0], blocPosition, Quaternion.Euler(blocRotation), canvas.transform);
-    }
+
 }
